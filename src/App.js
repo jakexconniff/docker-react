f5faba042634c5c6b8d055083c170ceeb89349f8
Jacob Conniff <jakexconniff@gmail.com>
2019-04-07 19:35:34 -0400

    change App.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>suh dude</h1>
+          <h1>DOCKER!!!!!!!!!!</h1>
         </header>
       </div>
     );
